983655b2c5184d340da38193c48d53da930a72bf
Jack Owen <jack@Jacks-MacBook-Air.local>
2025-04-09 20:54:27 +0100

    add input stream support

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,49 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
 
   public static void main(String[] args) {
+    try {
+      List<String> words = readWordsFromFile()
+          .stream()
+          .map(String::trim)
+          .filter(s -> !s.isEmpty())
+          .toList();
 
-    System.out.println(anagram("hi", "ih"));
-    System.out.println(anagram("h", "ih"));
+      for (int i = 0; i < words.size() - 1; i++) {
+        String word1 = words.get(i);
+        String word2 = words.get(i + 1);
+        System.out.printf("%s and %s are anagrams: %b%n",
+            word1, word2, anagram(word1, word2));
+      }
+    } catch (IOException e) {
+      System.err.println("Error reading words file: " + e.getMessage());
+    }
+  }
+
+  private static List<String> readWordsFromFile() throws IOException {
+    List<String> words = new ArrayList<>();
+    try (InputStream is = Main.class.getResourceAsStream("/words-utf8.txt")) {
+      if (is == null) {
+        throw new IllegalStateException("file not found words-utf8.txt");
+      }
+      try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+          words.add(line);
+        }
+      }
+    }
+    return words;
   }
 
   public static boolean anagram(String a, String b) {
