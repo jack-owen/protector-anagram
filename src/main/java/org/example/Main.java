e04550cac4e51c17d3a9971d4f6b54f2340363bf
Jack Owen <jack@Jacks-MacBook-Air.local>
2025-04-10 20:23:03 +0100

    find anagrams across set

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,12 +20,7 @@ public class Main {
           .filter(s -> !s.isEmpty())
           .toList();
 
-      for (int i = 0; i < words.size() - 1; i++) {
-        String word1 = words.get(i);
-        String word2 = words.get(i + 1);
-        System.out.printf("%s and %s are anagrams: %b%n",
-            word1, word2, anagram(word1, word2));
-      }
+      anagram(words);
     } catch (IOException e) {
       System.err.println("Error reading words file: " + e.getMessage());
     }
@@ -46,30 +42,22 @@ public class Main {
     return words;
   }
 
-  public static boolean anagram(String a, String b) {
-    if (a.length() != b.length()) {
-      return false;
+  private static void anagram(List<String> words) {
+    Map<String, List<String>> map = new HashMap<>();
+
+    for (String original : words) {
+      char[] chars = original.toLowerCase().toCharArray();
+      Arrays.sort(chars);
+      String sorted = Arrays.toString(chars);
+      map.computeIfAbsent(sorted, k -> new ArrayList<>()).add(original);
     }
 
-    Map<Character, Integer> map = new HashMap<>();
-
-    for (int i = 0; i < a.length(); i++) {
-      map.put(a.charAt(i), map.getOrDefault(a.charAt(i), 0) + 1);
-    }
-
-    for (int i = 0; i < b.length(); i++) {
-      if (!map.containsKey(b.charAt(i))) {
-        return false;
-      }
-
-      int count = map.get(b.charAt(i)) - 1;
-      if (count == 0) {
-        map.remove(b.charAt(i));
-      } else {
-        map.put(b.charAt(i), count);
+    for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+      if (entry.getValue().size() > 1) {
+        System.out.println(String.join(" ", entry.getValue()));
       }
     }
 
-    return map.isEmpty();
   }
+
 }
